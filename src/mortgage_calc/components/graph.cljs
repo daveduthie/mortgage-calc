8f085f0ca6b997fa574a312826c7840a95d61804
David Duthie <dave.a.duthie@gmail.com>
2018-05-05 10:00:45 +0200

    better (really?) animations for graphs

--- src/mortgage_calc/components/graph.cljs
+++ src/mortgage_calc/components/graph.cljs
@@ -5,47 +5,59 @@
    [mortgage-calc.util :as util]
    [reagent.core :as r]))
 
-(defn make-config
-  [splits]
-  (let [principals (mapv :principal-repaid splits)
-        interests  (mapv :interest-serviced splits)
-        labels     (take (count splits) (iterate inc 2019))]
-    (clj->js
-     {:type    "line"
-      :data    {:labels   labels
-                :datasets [{:label           "Principal repaid"
-                            :data            principals
-                            :backgroundColor "rgba(0, 255, 0, 0.4)"
-                            :borderWidth     1}
-                           {:label           "Interest serviced"
-                            :data            interests
-                            :backgroundColor "rgba(255, 0, 0, 0.4)"
-                            :borderWidth     1}]}
-      :options {:scales {:yAxes [{:stacked true}]}}})))
+(def config
+  (clj->js
+   {:type    "line"
+    :data    {:labels   []
+              :datasets [{:label           "Principal repaid"
+                          :data            []
+                          :backgroundColor "rgba(0, 245, 100, 0.6)"
+                          :borderWidth     1}
+                         {:label           "Interest serviced"
+                          :data            []
+                          :backgroundColor "rgba(255, 0, 0, 0.4)"
+                          :borderWidth     1}]}
+    :options {:scales {:yAxes [{:stacked true}]}}}))
+
+;; FIXME: this fiddling around inside a js object fails under advanced compilation
+(defn set-data!
+  [graph splits]
+  (let [labels         (take (count splits) (iterate inc 2019))
+        data           (.-data graph)
+        principal-data (-> data .-datasets (aget 0))
+        interest-data  (-> data .-datasets (aget 1))]
+    (set! (.-labels data) (clj->js labels))
+    (set! (.-data principal-data) (clj->js (mapv :principal-repaid splits)))
+    (set! (.-data interest-data) (clj->js (mapv :interest-serviced splits)))
+    (.update graph)))
 
 (defn graph-inner
   []
-  (let [local (r/atom nil)
+  (let [local (r/atom nil) ; normal atom?
         update
         (fn [comp]
+          (let [{:keys [splits]} (r/props comp)]
+            (set-data! (:graph @local) splits)))
+        render
+        (fn [comp]
           (let [{:keys [splits]} (r/props comp)
-                splits           (rest splits) ; time zero is always empty
                 labels           (take (count splits) (iterate inc 2019))
                 ctx              (-> js/document
                                      (.getElementById "graph")
                                      (.getContext "2d"))]
-            ;; Would prefer something like:
-            #_ (set! (-> @local :graph .-data) (make-config splits))
-            #_ (.update (:graph @local))
-            (if-let [graph (:graph @local)]
-              (.destroy graph))
-            (reset! local {:graph (js/Chart. ctx (make-config splits))})))]
+            (if-let [graph (:graph @local)] (.destroy graph))
+            (reset! local {:graph (js/Chart. ctx config)})
+            (update comp)))
+        destroy
+        (fn [comp]
+          (.destroy (:graph @local)))]
     (r/create-class
-     {:reagent-render       (fn [] [:canvas#graph.container])
-      :component-did-mount  update
-      :component-did-update update
-      :display-name         "graph"})))
+     {:reagent-render         (fn [] [:canvas#graph.container])
+      :component-did-mount    render
+      :component-did-update   update
+      :component-will-unmount destroy
+      :display-name           "graph"})))
 
 (defn graph-view
   [splits]
-  [graph-inner {:splits splits}])
+  [graph-inner {:splits (rest splits)}])
